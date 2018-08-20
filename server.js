a11bfd543417a719365e962fbb5dd6c6da8a5caf
Corey Johnson <probablycorey@github.com>
2018-08-20 14:22:32 -0700

    More stuff

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
   console.log("sending another lulz")
-  res.send("YOU SHIT")
+  res.send("WORK BABY")
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
